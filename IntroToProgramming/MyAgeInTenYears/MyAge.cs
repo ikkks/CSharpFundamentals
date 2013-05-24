3a069455ae9dac800237fc04015c637168da2396
I.K. <sneizzzyg@yahoo.com>
2013-05-24 20:10:04 +0300

    IntroToProgramming

---
+++ IntroToProgramming/MyAgeInTenYears/MyAge.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class MyAge
+{
+    static void Main()
+    {
+        Console.Write("Your age:");
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Your age in 10 years will be: {0}", age + 10);
+    }
+}
---
+++ IntroToProgramming/PrintFirst10Numbers/PrintFirst10Numbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class PrintFirst10Numbers
+{
+    static void Main()
+    {
+        int[] numArray = { 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12, -13, 14 };
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(numArray[i]);
+        }
+    }
+}
+
